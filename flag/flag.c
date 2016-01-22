a5b7eb086e7fc339cc7f4e75558c1821f5cdcaeb
Anagh Singh <anaghsingh.nitk@gmail.com>
2016-01-22 15:52:33 +0530

    Add chakra with strikethroughs

--- flag/flag.c
+++ flag/flag.c
@@ -1,28 +1,40 @@
 // Flag of India
 #include <stdio.h>
 #include <graphics.h>
-void lineDDA(int, int, int, int, int);
+#include <math.h>
+// #include <time.h>
+void DDAdraw(int, int, int, int, int);
+void calcPoints(int, int, int, int *, int *);
 void main()
 {
   int x1, y1, x_n, y_n,i;
   int gd = DETECT, gm;
   initgraph(&gd, &gm, "");
+  int x[12],y[12];
 
   //For the 3 bars of the flag
-  for (i = 0; i < 20; i++) {
+  for (i = 0; i < 60; i++) {
     /* code */
-    lineDDA(20,20+i,100,20+i,0);
-    lineDDA(20,40+i,100,40+i,1);
-    lineDDA(20,60+i,100,60+i,2);
-    lineDDA(20+i/5,20,20+i/5,200,3);
-  }
-  //lineDDA(20,20,20,200,3);
+    DDAdraw(100,20+i,500,20+i,0);
+    DDAdraw(100,80+i,500,80+i,1);
+    DDAdraw(100,140+i,500,140+i,2);
+    // DDAdraw(10+i/30,200,10+i/30,200,3);
 
-  int color = getpixel(25,25);
-//  printf("%d\n",color );
-  delay(2000);
+    setcolor(BLUE);
+    circle(300, 110, 30);
+  }
+  // For the bars of the circle
+  calcPoints(30, 300, 110, x, y);
+  for (i = 0; i < 12; i++)
+  {
+     if (i % 2 == 0)
+        line(300,110, x[i], y[i]);
+   }
+
+delay(2000);
+  //getch();
 }
-void lineDDA(int x1, int y1, int x_n, int y_n, int k)
+void DDAdraw(int x1, int y1, int x_n, int y_n, int k)
 {
   int dx, dy, m, i;
 
@@ -70,5 +82,36 @@ void lineDDA(int x1, int y1, int x_n, int y_n, int k)
       putpixel(x1, y1, k==0?LIGHTRED:(k==1?WHITE:(k==2?GREEN:YELLOW)));
     }
   }
+}
 
+// For finding the points at 0, 30, 60...360 degrees
+void calcPoints(int radius, int midx, int midy, int x[12], int y[12])
+{
+      // midx= x coordinate of circle
+      // midy= y coordinate of circle
+      int x1, y1;
+
+      // 90, 270, 0, 180 degrees
+      x[0] = midx, y[0] = midy - radius;
+      x[6] = midx, y[6] = midy + radius;
+      x[3] = midx + radius, y[3] = midy;
+      x[9] = midx - radius, y[9] = midy;
+
+      // 30, 150, 210, 330 degrees
+      x1 = (int) ((radius / 2) * sqrt(3));
+      y1 = (radius / 2);
+      x[2] = midx + x1, y[2] = midy - y1;
+      x[4] = midx + x1, y[4] = midy + y1;
+      x[8] = midx - x1, y[8] = midy + y1;
+      x[10] = midx - x1, y[10] = midy - y1;
+
+      // 60, 120, 210, 300 degrees
+      x1 = radius / 2;
+      y1 = (int) ((radius / 2)  * sqrt(3));
+      x[1] = midx + x1, y[1] = midy - y1;
+      x[5] = midx + x1, y[5] = midy + y1;
+      x[7] = midx - x1, y[7] = midy + y1;
+      x[11] = midx - x1, y[11] = midy - y1;
+
+      return ;
 }
