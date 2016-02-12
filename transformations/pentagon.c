27a02830509417241228be1115152328b70d4936
Anagh Singh <anaghsingh.nitk@gmail.com>
2016-02-12 14:32:31 +0530

    Add option for user scaling

--- transformations/pentagon.c
+++ transformations/pentagon.c
@@ -3,146 +3,116 @@
 
 int main()
 {
-     	int gd=DETECT,gm,choice,z,si,co;
+     int gd=DETECT,gm,choice,z,si,co;
 
 
-   	printf("Menu:\n1.Transformation\n2.Scaling\n3.Rotation\n Enter your choice    ");
-	scanf("%d",&choice);
-  
-	
+   	 printf("Menu:\n1.Transformation\n2.Scaling\n3.Rotation\n Enter your choice    ");
+	   scanf("%d",&choice);
+
+
 
  	switch(choice)
 	{
 	   case 1:
- 
-		initgraph(&gd,&gm,NULL);
-		
-		setcolor(CYAN);
 
-		line(200,100,105,169);
-		line(105,169,141,281);
-	 	line(141,281,259,281);
-		line(259,281,295,169);
-		line(295,169,200,100);
+        		initgraph(&gd,&gm,NULL);
 
-		delay(1000);
-		cleardevice();
-		
- 		for(si=0;si<=50;si+=10)
-		{
-			cleardevice();
-			line(200+si,100+si,105+si,169+si);
-			line(105+si,169+si,141+si,281+si);
-		 	line(141+si,281+si,259+si,281+si);
-			line(259+si,281+si,295+si,169+si);
-			line(295+si,169+si,200+si,100+si);
-			delay(500);
+        		setcolor(CYAN);
 
-			
-  		}
-	 	delay(5000);
-		closegraph();
+        		line(200,100,105,169);
+        		line(105,169,141,281);
+        	 	line(141,281,259,281);
+        		line(259,281,295,169);
+        		line(295,169,200,100);
 
-		break;
+        		delay(1000);
+        		cleardevice();
 
-	
-           case 2:
- 
-		printf("\n\n1.Zoom in\n2.Zoom out\n Choice   ");
-		scanf("%d",&z);
-
-		if(z==1)
- 		{
-		
-			initgraph(&gd,&gm,NULL);
-		
-			setcolor(CYAN);
-
-			line(200,100,105,169);
-			line(105,169,141,281);
-		 	line(141,281,259,281);
-			line(259,281,295,169);
-			line(295,169,200,100);
+         		for(si=0;si<=50;si+=10)
+        		{
+        			cleardevice();
+        			line(200+si,100+si,105+si,169+si);
+        			line(105+si,169+si,141+si,281+si);
+        		 	line(141+si,281+si,259+si,281+si);
+        			line(259+si,281+si,295+si,169+si);
+        			line(295+si,169+si,200+si,100+si);
+        			delay(500);
 
 
-			 delay(1000);
-			cleardevice();
-		
-			line(200,50,57,154);
-			line(57,154,112,321);
-		 	line(112,321,288,321);
-			line(288,321,343,154);
-			line(343,154,200,50);
+          		}
+        	 	delay(5000);
+        		closegraph();
 
-		 	delay(5000);
-			closegraph();
-
-			break;
-                 }
-
-		if(z==2)
- 		{
-			initgraph(&gd,&gm,NULL);
-		
-			setcolor(CYAN);
-
-			line(200,100,105,169);
-			line(105,169,141,281);
-		 	line(141,281,259,281);
-			line(259,281,295,169);
-			line(295,169,200,100);
+        		break;
 
 
-			 delay(1000);
-			cleardevice();
-		
-			line(200,150,152,185);
-			line(152,185,171,240);
-		 	line(171,240,229,240);
-			line(229,240,248,185);
-			line(248,185,200,150);
+    case 2:
 
-		 	delay(5000);
-			closegraph();
+        		printf("\n\n1.X-axis scale\n2.Y-axis scale\nEnter your choices:   ");
+        		int z1,z2;
 
-			break;
-                 }   
+            scanf("%d",&z1);  // For specific X-scaling
+            scanf("%d",&z2);  // For specific Y-scaling
 
-		break;
 
-	   
+        			initgraph(&gd,&gm,NULL);
+
+        			setcolor(CYAN);
+
+        			line(200,100,105,169);
+        			line(105,169,141,281);
+        		 	line(141,281,259,281);
+        			line(259,281,295,169);
+        			line(295,169,200,100);
+
+
+        			delay(1000);
+        			cleardevice();
+
+        			line(200*z1,50*z2,57*z1,154*z2);
+        			line(57*z1,154*z2,112*z1,321*z2);
+        		 	line(112*z1,321*z2,288*z1,321*z2);
+        			line(288*z1,321*z2,343*z1,154*z2);
+        			line(343*z1,154*z2,200*z1,50*z2);
+
+        		 	delay(5000);
+        			closegraph();
+
+        			break;
+
 	   case 3:
- 
-		initgraph(&gd,&gm,NULL);
-		
-		setcolor(CYAN);
 
-		line(200,100,105,169);
-		line(105,169,141,281);
-	 	line(141,281,259,281);
-		line(259,281,295,169);
-		line(295,169,200,100);
+        		initgraph(&gd,&gm,NULL);
 
-		delay(1000);
-		cleardevice();
-		
-		line(200*cos(15*3.1414/180)-100*sin(15*3.1414/180),200*sin(15*3.1414/180)+100*cos(15*3.1414/180),105*cos(15*3.1414/180)-169*sin(15*3.1414/180),105*sin(15*3.1414/180)+169*cos(15*3.1414/180));
-			
-			line(105*cos(15*3.1414/180)-169*sin(15*3.1414/180),105*sin(15*3.1414/180)+169*cos(15*3.1414/180),141*cos(15*3.1414/180)-281*sin(15*3.1414/180),141*sin(15*3.1414/180)+281*cos(15*3.1414/180));
+        		setcolor(CYAN);
 
-		 	line(141*cos(15*3.1414/180)-281*sin(15*3.1414/180),141*sin(15*3.1414/180)+281*cos(15*3.1414/180),259*cos(15*3.1414/180)-281*sin(15*3.1414/180),259*sin(15*3.1414/180)+281*cos(15*3.1414/180));
+        		line(200,100,105,169);
+        		line(105,169,141,281);
+        	 	line(141,281,259,281);
+        		line(259,281,295,169);
+        		line(295,169,200,100);
 
-			line(259*cos(15*3.1414/180)-281*sin(15*3.1414/180),259*sin(15*3.1414/180)+281*cos(15*3.1414/180),295*cos(15*3.1414/180)-169*sin(15*3.1414/180),295*sin(15*3.1414/180)+169*cos(15*3.1414/180));
+        		delay(1000);
+        		cleardevice();
 
-			line(295*cos(15*3.1414/180)-169*sin(15*3.1414/180),295*sin(15*3.1414/180)+169*cos(15*3.1414/180),200*cos(15*3.1414/180)-100*sin(15*3.1414/180),200*sin(15*3.1414/180)+100*cos(15*3.1414/180));
+            line(200*cos(15*3.1414/180)-100*sin(15*3.1414/180),200*sin(15*3.1414/180)+100*cos(15*3.1414/180),105*cos(15*3.1414/180)-169*sin(15*3.1414/180),105*sin(15*3.1414/180)+169*cos(15*3.1414/180));
 
-	 	delay(5000);
-		closegraph();
+      			line(105*cos(15*3.1414/180)-169*sin(15*3.1414/180),105*sin(15*3.1414/180)+169*cos(15*3.1414/180),141*cos(15*3.1414/180)-281*sin(15*3.1414/180),141*sin(15*3.1414/180)+281*cos(15*3.1414/180));
 
-		break;
+      		 	line(141*cos(15*3.1414/180)-281*sin(15*3.1414/180),141*sin(15*3.1414/180)+281*cos(15*3.1414/180),259*cos(15*3.1414/180)-281*sin(15*3.1414/180),259*sin(15*3.1414/180)+281*cos(15*3.1414/180));
 
-	  default: printf("Invalid choice\n"); 
+      			line(259*cos(15*3.1414/180)-281*sin(15*3.1414/180),259*sin(15*3.1414/180)+281*cos(15*3.1414/180),295*cos(15*3.1414/180)-169*sin(15*3.1414/180),295*sin(15*3.1414/180)+169*cos(15*3.1414/180));
+
+      			line(295*cos(15*3.1414/180)-169*sin(15*3.1414/180),295*sin(15*3.1414/180)+169*cos(15*3.1414/180),200*cos(15*3.1414/180)-100*sin(15*3.1414/180),200*sin(15*3.1414/180)+100*cos(15*3.1414/180));
+
+
+        	 	delay(5000);
+        		closegraph();
+
+		        break;
+
+	  default: printf("Invalid choice\n");
 	}
 
-   return 0;
+  return 0;
 }
--- transformations/staticcube.c
+++ transformations/staticcube.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-#include<graphics.h>
+#include <stdio.h>
+#include <graphics.h>
 
 int main()
 {
      	int i,gd=DETECT,gm;
 	initgraph(&gd,&gm,NULL);
-	
+
 	setcolor(CYAN);
 
 	line(200,200,300,200);
 	line(300,200,300,100);
 	line(200,200,200,100);
 	line(200,100,300,100);
-	
+
 	line(250,250,350,250);
 	line(350,250,350,150);
 	line(250,250,250,150);
@@ -29,7 +29,7 @@ int main()
 
 	setcolor(14);
 	for(i=0;i<50;i++)
-	   line(200+i,100+i,200+i,200+i);	
+	   line(200+i,100+i,200+i,200+i);
 
  	delay(10000);
 	closegraph();
